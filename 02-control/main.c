ceb6e128bc4f724c3c574f6592d0aef5596bfb2c
Lew L3V Podmoskovnov <lev.tmb.l29@gmail.com>
2026-02-19 16:02:41 +0300

    half 2 task

--- 02-control/main.c
+++ 02-control/main.c
@@ -1,5 +1,46 @@
 #include "stdio-task/stdio-task.h"
 #include "pico/stdlib.h"
+#include "protocol-task/protocol-task.h"
+#include "led-task/led-task.h"
+#include "stdio.h"
+
+#define DEVICE_NAME "my-pico-device"
+#define DEVICE_VRSN "v0.0.1"
+
+void version_callback(const char *args)
+{
+    printf("device name: '%s', firmware version: %s\n", DEVICE_NAME, DEVICE_VRSN);
+}
+
+void led_on_callback(const char *args)
+{
+    led_state_t state = LED_STATE_ON;
+    led_task_state_set(state);
+    printf("LED On\n");
+}
+
+void led_off_callback(const char *args)
+{
+    led_state_t state = LED_STATE_OFF;
+    led_task_state_set(state);
+    printf("LED Off\n");
+}
+
+void led_blink_callback(const char *args)
+{
+    led_state_t state = LED_STATE_BLINK;
+    led_task_state_set(state);
+    printf("LED Blink\n");
+}
+
+api_t device_api[] =
+    {
+        {"version", version_callback, "get device name and firmware version"},
+        {"on", led_on_callback, "led on"},
+        {"off", led_off_callback, "led off"},
+        {"blink", led_blink_callback, "led blink"},
+        {NULL, NULL, NULL},
+};
 
 int main()
 {
@@ -7,8 +48,16 @@ int main()
 
     stdio_task_init();
 
+    protocol_task_init(device_api);
+
+    led_task_init();
+
     while (1)
     {
-        stdio_task_handle();
+        // stdio_task_handle();
+
+        protocol_task_handle(stdio_task_handle());
+
+        led_task_handle();
     }
 }
--- 02-control/protocol-task/protocol-task.c
+++ 02-control/protocol-task/protocol-task.c
@@ -7,8 +7,6 @@ static int commands_count = 0;
 
 void protocol_task_init(api_t *device_api);
 
-void protocol_task_handle(char *command_string);
-
 void protocol_task_init(api_t *device_api)
 {
     api = device_api;
@@ -16,15 +14,24 @@ void protocol_task_init(api_t *device_api)
 }
 
 void protocol_task_handle(char *command_string)
+
 {
-    if (command_string == NULL)
+
+    //	Добавляем в обработчик проверку на то, что `command_string` не равно `NULL`.
+    //	Если `command_string` равно `NULL`, то выйти из обработчика: строка команды
+    //	еще не получена;
+    if (!command_string)
     {
+        // ваш код
         return;
     }
+
+    // логика обработки полученной строки. Делим ее на команду и аргументы:
     const char *command_name = command_string;
     const char *command_args = NULL;
 
     char *space_symbol = strchr(command_string, ' ');
+
     if (space_symbol)
     {
         *space_symbol = '\0';
@@ -35,5 +42,24 @@ void protocol_task_handle(char *command_string)
         command_args = "";
     }
 
-    printf("command name: '%s', command args: %s\n", command_name, command_args);
+    // Добавляем вывод найденных имени команды и ее аргументов;
+    // ваш код
+
+    // в цикле проходим по массиву команд `api` и ищем совпадение имени команды;
+
+    for (int i = 0; i < commands_count; i++)
+    {
+        // определяем совпадает ли команда  с именем команды в массиве `api`
+        // если не совпадает, переходим к следующей итерации
+        if (!strcmp(command_name, api[i].command_name))
+        {
+            api[i].command_callback(command_args);
+            return;
+        }
+        // мы нашли команду, вызываем callback найденной команды
+    }
+    // выводим ошибку, если команда не была найдена в списке команд
+    // ваш код
+    printf("Command not found\n");
+    return;
 }
